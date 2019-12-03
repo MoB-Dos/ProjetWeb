3e86f29109b2e56c3feb34eda18ef90c13e8cfd9
Errazorr <n.goncalves@lprs.fr>
2019-12-03 09:12:44 +0100

    Connexion

---
+++ connexion.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+
+<form method="post" action="Session_1.php">
+
+	Votre nom:
+	<input type="text" name="login" required/>
+
+  Votre prenom:
+	<input type="text" name="mdp" required/>
+
+	<input type="submit" value="Connexion"/>
+  <input type="button" value="Inscription" onclick="window.location.href='Inscription.php'">
+
+
+</form>
