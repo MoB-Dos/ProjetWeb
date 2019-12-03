a6a7e051c0d2b343ab176b8ff1211c4e6317afbc
Kikichaton <K.KEBICHE@lprs.fr>
2019-12-03 11:25:47 +0100

    g

--- Accueil2.php
+++ Accueil2.php
@@ -1,2 +1,4 @@
 <?php echo "Bonjour" ?><br>
 <input type="button" value="se déconecter" onclick="window.location.href='Accueil.php'">
+<input type="button" value="modifier" onclick="window.location.href='modification.php'">
+<input type="button" value="Afficher les données" onclick="window.location.href='afficher.php'">
