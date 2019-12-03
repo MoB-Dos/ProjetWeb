fafb0f8f9634f18f85e0735f7a5210ae27fa770f
Errazorr <n.goncalves@lprs.fr>
2019-12-03 09:44:34 +0100

    Modif form co

--- connexion.php
+++ connexion.php
@@ -7,8 +7,8 @@ session_start();
 	Votre nom:
 	<input type="text" name="login" required/>
 
-  Votre prenom:
-	<input type="text" name="mdp" required/>
+  Votre mot de passe:
+	<input type="password" name="mdp" required/>
 
 	<input type="submit" value="Connexion"/>
   <input type="button" value="Inscription" onclick="window.location.href='Inscription1.php'">
