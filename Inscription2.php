804d7846e3b08713cea65ff55fd2a6c02635a0b5
Errazorr <n.goncalves@lprs.fr>
2019-12-04 11:48:46 +0100

    Tentative

--- Inscription2.php
+++ Inscription2.php
@@ -4,7 +4,7 @@ session_start ();
 
 $nom=$_POST['nom'];
 $prenom=$_POST['prenom'];
-$mail=$_POST['mail'];
+$email=$_POST['mail'];
 $tel=$_POST['tel'];
 $adresse=$_POST['adresse'];
 $classe=$_POST['classe'];
@@ -28,7 +28,7 @@ catch(Exception $e){
 }
 
 $reponse=$bdd->prepare('SELECT * FROM utilisateur WHERE nom=? AND prenom=? OR mail=?');
-$reponse->execute(array($nom, $prenom,$mail));
+$reponse->execute(array($nom, $prenom,$email));
 $donne=$reponse->fetchall();
 
 if ($donne) {
@@ -40,7 +40,45 @@ if ($donne) {
 else {
   if ($mdp == $mdp2) {
     $req = $bdd->prepare('INSERT INTO utilisateur (nom, prenom, mail, tel, adresse, classe, profil_id, mdp) VALUES (?,?,?,?,?,?,?,?)');
-    $req -> execute(array($nom, $prenom, $mail, $tel, $adresse, $classe, $profil_id, $mdp));
+    $req -> execute(array($nom, $prenom, $email, $tel, $adresse, $classe, $profil_id, $mdp));
+    $email = $_GET['mail'];
+
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
+
+    require 'vendor/phpmailer/phpmailer/src/Exception.php';
+    require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+    require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+    require 'vendor/autoload.php';
+
+    $mail = new PHPMailer(true);
+
+    try {
+
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'projetweb932@gmail.com';
+        $mail->Password   = 'projetweb932';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
+
+        $mail->setFrom('projetweb932@gmail.com', 'Mailer');
+        $mail->addAddress($email, 'user');
+
+        $mail->isHTML(true);
+        $mail->Subject = 'test';
+        $mail->Body    = 'regarder ça marche!';
+        $mail->AltBody = 'regarder ça marche!';
+
+        $mail->send();
+        echo 'Message has been sent';
+          } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+          }
     header("location:Connexion.php");
   }
 
