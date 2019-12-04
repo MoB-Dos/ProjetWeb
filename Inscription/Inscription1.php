0e1aca38ddd052e95c325d5c93187662fab5f608
Errazorr <n.goncalves@lprs.fr>
2019-12-04 17:13:30 +0100

    Liste deroulante

--- Inscription/Inscription1.php
+++ Inscription/Inscription1.php
@@ -22,21 +22,24 @@ session_start();
 	<input type="text" name="adresse"><br><br>
 
 	Votre classe:
-    	<select name="classe">
-      <option value="first">First</option>
-      <option value="second">Second</option>
-      <option value="third">Third</option>
-			<option value="first">First</option>
-      <option value="second">Second</option>
-      <option value="third">Third</option>
-			<option value="first">First</option>
-      <option value="second">Second</option>
-      <option value="third">Third</option>
-			<option value="first">First</option>
-      <option value="second">Second</option>
-      <option value="third">Third</option>
-</select>
+	<select name="classe">
+	<?php
+	try{
+	$bdd= new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8','root','');
+	}
 
+	catch(Exception $e){
+		die('Erreur:'.$e->getMessage());
+	}
+
+	$reponse=$bdd->query('SELECT classe FROM classe');
+	$donne=$reponse->fetchall();
+	foreach ($donne as $value) {
+		echo '<option>'.$value['classe'].'</option>';
+	}
+
+	?>
+</select><br><br>
 Votre profil:
 	<select name="profil_id">
 			<option>etudiant</option>;
