8ae1894a5c5f53ed43058f21d784727410168141
Kikichaton <K.KEBICHE@lprs.fr>
2019-12-06 11:58:17 +0100

    Postuler

---
+++ postuler/postuler.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<hmtl>
+  <head><form method="post" action="postuler2.php">
+  </head>
+  <body>
+    <h3>Responsable</h3>
+      Information personnelles
+  Civilité : <input type="text" name="civilité"><br><br>
+  nom : <input type="text" name="nom"><br><br>
+  prénom : <input type="text" name="prenom"><br><br>
+  âge : <input type="text" name="age"><br><br>
+  Adresse : <input type="text" name="adresse"><br><br>
+  Code postal : <input type="text" name="codepostale"><br><br>
+  Pays : <input type="text" name="Pays"><br><br>
+  Téléphone Mobile : <input type="text" name="telephone Mobile"><br><br>
+  Téléphone portable : <input type="text" name="telephone portable"><br><br>
+  Email : <input type="text" name="mail"><br><br>
+  Confimez votre Email : <input type="text" name="mail2"><br><br>
+
+    Information Professionelles
+  Profession : <input type="text" name="profession"><br><br>
+  Société : <input type="text" name="societe"><br><br>
+  Téléphone : <input type="text" name="telpro"><br><br>
+  Catégorie Socio-Professionelle : <input type="text" name="sociopro"><br><br>
+  Situation Familliale : <input type="text" name="Sitfamille"><br><br>
+
+  <h3> Renseignement Eleves </h3>
+  Nom : <input type="text" name="nomeleve"><br><br>
+  Prénom : <input type="text" name="prenomeleve"><br><br>
+  Autres Prénoms : <input type="text" name="prenom2eleve"><br><br>
+  Civillité : <input type="text" name="civeleve"><br><br>
+  Lien de parenté : <input type="text" name="parente"><br><br>
+
+  <h3> Communication </h3>
+  Email : <input type="text" name="maileleve"><br><br>
+  Téléphone mobile : <input type="text" name="teleleve"><br><br>
+
+  <h3> Naissance </h3>
+  Date : <input type="date" name="dateeleve"><br><br>
+  Ville : <input type="text" name="villeeleve"><br><br>
+  Pays : <input type="text" name="payseleve"><br><br>
+  Nationalité : <input type="text" name="natioeleve"><br><br>
+
+  <h3> Scolarité </h3>
+  Etablisement actuel : <input type="text" name="etaactu"><br><br>
+  Formation actuel : <input type="text" name="Formaactu"><br><br>
+  Inscription Pour : <input type="date" name="dateinscri"><br><br>
+  Régime souhaité : <input type="text" name="cantine"><br><br>
+  <h3> Voeux <h3>
+  Classe souhaitée : <input type="text" name="classe"><br><br>
+  <input type="submit" name="Valider"><br><br
+  </body>
+  </html>
