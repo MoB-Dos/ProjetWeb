6339f21b9b73e1724da1e5b548a1a7be0fdb2333
Errazorr <n.goncalves@lprs.fr>
2019-12-09 12:18:44 +0100

    modifs style affichage (encore)

--- Affichage/afficher.php
+++ Affichage/afficher.php
@@ -17,13 +17,13 @@ $donne=$req->fetchall();
 //Affichage de chacune des données selon le profil_id
 foreach ($donne as $value) {
   if($value['profil_id'] == '1'){
-    echo $value['nom']." ".$value['prenom'].'<br><br>'."Mail :"." ".$value['mail'].'<br><br>'."Téléphone :"." ".$value['tel'].'<br><br>'."Adresse :"." ".$value['adresse'].'<br><br>'."Classe :"." ".$value['classe'].'<br><br>'."Etudiant";
+    echo $value['nom']." ".$value['prenom'].'<br><br>'."Mail :"." ".$value['mail'].'<br><br>'."Téléphone :"." ".$value['tel'].'<br><br>'."Adresse :"." ".$value['adresse'].'<br><br>'."Etudiant en :"." ".$value['classe'];
   }
   if($value['profil_id'] == '2'){
-    echo $value['nom']." ".$value['prenom'].'<br><br>'."Mail :"." ".$value['mail'].'<br><br>'."Téléphone :"." ".$value['tel'].'<br><br>'."Adresse :"." ".$value['adresse'].'<br><br>'."Classe :"." ".$value['classe'].'<br><br>'."Parent";
+    echo $value['nom']." ".$value['prenom'].'<br><br>'."Mail :"." ".$value['mail'].'<br><br>'."Téléphone :"." ".$value['tel'].'<br><br>'."Adresse :"." ".$value['adresse'].'<br><br>'."Parent d'un élève en :"." ".$value['classe'];
   }
   if($value['profil_id'] == '3'){
-    echo $value['nom']." ".$value['prenom'].'<br><br>'."Mail :"." ".$value['mail'].'<br><br>'."Téléphone :"." ".$value['tel'].'<br><br>'."Adresse :"." ".$value['adresse'].'<br><br>'."Classe :"." ".$value['classe'].'<br><br>'."Admin";
+    echo $value['nom']." ".$value['prenom'].'<br><br>'."Mail :"." ".$value['mail'].'<br><br>'."Téléphone :"." ".$value['tel'].'<br><br>'."Adresse :"." ".$value['adresse'].'<br><br>'." "."Administrateur";
   }
 }
 ?>
