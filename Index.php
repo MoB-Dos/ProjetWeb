699658c5f0355434589ebf61207c59a579b165f8
Errazorr <n.goncalves@lprs.fr>
2019-12-09 10:44:15 +0100

    modif nom fichiers

--- Index.php
+++ Index.php
@@ -1,2 +1,2 @@
 <input type="button" value="Se connecter" onclick="window.location.href='http://localhost/Projet/GIT/ProjetWeb/Connexion/connexion.php'">
-<input type="button" value="Inscription" onclick="window.location.href='http://localhost/Projet/GIT/ProjetWeb/Inscription/Inscription1.php'">
+<input type="button" value="Inscription" onclick="window.location.href='http://localhost/Projet/GIT/ProjetWeb/Inscription/Inscription.php'">
--- Modification/TraitementModif.php
+++ Modification/TraitementModif.php
@@ -19,25 +19,8 @@ elseif ($profil_id=='parent') {
   $profil_id='2';
 }
 
-if ($mdp == $mdp2) {
-  try{
-  $bdd= new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8','root','');
-  }
-
-  catch(Exception $e){
-    die('Erreur:'.$e->getMessage());
-  }
-
   $req = $bdd->prepare('UPDATE utilisateur SET nom = ?, prenom = ?, mail = ?, tel = ?, adresse = ?, classe = ?  WHERE nom= ? AND prenom = ?');
   $a = $req -> execute(array($nom, $prenom, $mail, $tel, $adresse, $classe, $_SESSION['login'], $_SESSION['prenom']));
   header("location:http://localhost/Projet/GIT/ProjetWeb/Connexion/connexion.php");
-}
-
-else {
-  echo '<body onLoad="alert(\'Veuillez entrer deux mots de passe identiques\')">';
-
-  echo '<meta http-equiv="refresh" content="0;URL=http://localhost/Projet/GIT/ProjetWeb/Inscription/Inscription1.php">';
-}
-
 
 ?>
