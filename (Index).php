9680f26a66f415e51b9b7021c0d466c4edf44640
Errazorr <n.goncalves@lprs.fr>
2019-12-10 10:14:19 +0100

    Modif index

--- (Index).php
+++ (Index).php
@@ -1,2 +1,5 @@
-<input type="button" value="Se connecter" onclick="window.location.href='http://localhost/Projet/GIT/ProjetWeb/Connexion/connexion.php'">
-<input type="button" value="Inscription" onclick="window.location.href='http://localhost/Projet/GIT/ProjetWeb/Inscription/Inscription.php'">
+<?php
+
+header("location:http://localhost/Projet/GIT/ProjetWeb/TemplateConnexion/ConnexionTem.php")
+
+ ?>
