d22a2d196e0717127eb2b98d4bf7b11125a0fcf7
Errazorr <n.goncalves@lprs.fr>
2019-12-10 09:35:23 +0100

    modif chemin déco

--- Accueil/AccueilParent.php
+++ Accueil/AccueilParent.php
@@ -3,6 +3,6 @@
 session_start();
 //Affichage du texte et des boutons
 echo "Bonjour"." ".$_SESSION['login']." ".$_SESSION['prenom'] ?><br>
-<input type="button" value="Se déconnecter" onclick="window.location.href='http://localhost/Projet/GIT/ProjetWeb/Index.php'">
+<input type="button" value="Se déconnecter" onclick="window.location.href='http://localhost/Projet/GIT/ProjetWeb/TemplateConnexion/ConnexionTem.html'">
 <input type="button" value="Modifier vos données" onclick="window.location.href='http://localhost/Projet/GIT/ProjetWeb/Modification/modification.php'">
 <input type="button" value="Afficher vos données" onclick="window.location.href='http://localhost/Projet/GIT/ProjetWeb/Affichage/afficher.php'">
