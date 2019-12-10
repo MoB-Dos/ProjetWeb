ee9078260db2d231dd2bb6c3ffc0c1e2f37f6314
Errazorr <n.goncalves@lprs.fr>
2019-12-10 09:32:35 +0100

    ca marche

--- Affichage/afficher.php
+++ Affichage/afficher.php
@@ -1,6 +1,9 @@
 <?php
 //Demarrage de la session
 session_start();
+$lieneleve = "'../Accueil/AccueilEleve.php'";
+$lienparent = "'../Accueil/AccueilEleve.php'";
+$lienadmin = "'../Accueil/AccueilEleve.php'";
 //Connexion à la base de donnees projetweb
 try{
 $bdd= new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8','root','');
@@ -19,16 +22,15 @@ foreach ($donne as $value) {
   if($value['profil_id'] == '1'){
     echo $value['nom']." ".$value['prenom'].'<br><br>'."Mail :"." ".$value['mail'].'<br><br>'."Telephone :"." ".$value['tel'].'<br><br>'."Adresse :"." ".$value['adresse'].'<br><br>'."Etudiant en :"." ".$value['classe'];
     //Bouton de retour
-    $lien = "'../Accueil/AccueilEleve.php'";
-    echo '<br><br><input type="button" value="Retour" onclick="window.location.href='.$lien.'"/>';
+    echo '<br><br><input type="button" value="Retour" onclick="window.location.href='.$lieneleve.'"/>';
   }
   if($value['profil_id'] == '2'){
     echo $value['nom']." ".$value['prenom'].'<br><br>'."Mail :"." ".$value['mail'].'<br><br>'."Telephone :"." ".$value['tel'].'<br><br>'."Adresse :"." ".$value['adresse'].'<br><br>'."Parent d'un elève en :"." ".$value['classe'];
-    echo '<br><br><input type="button" value="Retour" onclick="window.location.href=http://localhost/Projet/GIT/ProjetWeb/Accueil/AccueilParent.php"/>';
+    echo '<br><br><input type="button" value="Retour" onclick="window.location.href='.$lienparent.'"/>';
   }
   if($value['profil_id'] == '3'){
     echo $value['nom']." ".$value['prenom'].'<br><br>'."Mail :"." ".$value['mail'].'<br><br>'."Telephone :"." ".$value['tel'].'<br><br>'."Adresse :"." ".$value['adresse'].'<br><br>'." "."Administrateur";
-    echo '<br><br><input type="button" value="Retour" onclick="window.location.href=http://localhost/Projet/GIT/ProjetWeb/Accueil/AccueilAdmin.php"/>';
+    echo '<br><br><input type="button" value="Retour" onclick="window.location.href='.$lienadmin.'"/>';
   }
 }
 ?>
