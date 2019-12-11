699c8bc69dbf713cc2288e748aca55ef23712a8a
Errazorr <n.goncalves@lprs.fr>
2019-12-11 09:56:15 +0100

    wut

--- (Index).php
+++ (Index).php
@@ -1,5 +1,53 @@
-<?php
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css'>
+<!--- Include the above in your HEAD tag ---------->
 
-header("location:http://localhost/Projet/GIT/ProjetWeb/Accueil/Accueil.php")
-
- ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Index</title>
+      <link rel="stylesheet" href="Css.css">
+      <script  src="./JS.js"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+   </head>
+   <body>
+      <div class="wrapper">
+         <header>
+            <nav>
+               <div class="menu-icon">
+                  <i class="fa fa-bars fa-2x"></i>
+               </div>
+               <div class="logo">
+                 <p>
+                   <a href="#"><img src="Image/Logo1.png" alt="ton image"/></a>
+                  </p>
+               </div>
+               <div class="menu">
+                  <ul>
+                     <li><a href="#"><span class="fa fa-home"></span> Acceuil</a></li>
+                     <li><a href="#"><span class="fas fa-graduation-cap"></span>Formations</a></li>
+                     <li><a href="#"><span class="fas fa-building"></span> Relations entreprises</a></li>
+                     <li><a href="#"><span class="fas fa-map-marker-alt"></span> Vie de l'établisement</a></li>
+                     <li><a href="http://localhost/Projet/GIT/ProjetWeb/TemplateConnexion/ConnexionTem.html"><span class="fas fa-sign-in-alt"> </span> Connexion</a></li>
+                     <li><a href="#"><span class="fas fa-sign-in-alt"> </span> Inscription</a></li>
+                  </ul>
+               </div>
+            </nav>
+         </header>
+         <div class="content">
+            <p>
+               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+            <p>
+               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+         </div>
+      </div>
+   </body>
+</html>
