7c85c999fe77c0c5565dccce56d70664f53d1934
Errazorr <56584432+Errazorr@users.noreply.github.com>
2019-12-21 15:51:19 +0100

    suppression fichiers qui servent a rien

--- Accueil/AccueilParent.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Démarrage de la session
-session_start();
-//Affichage du texte et des boutons
-echo "Bonjour"." ".$_SESSION['login']." ".$_SESSION['prenom'] ?><br>
-<input type="button" value="Se déconnecter" onclick="window.location.href='http://localhost/Projet/GIT/ProjetWeb/TemplateConnexion/ConnexionTem.html'">
-<input type="button" value="Modifier vos données" onclick="window.location.href='http://localhost/Projet/GIT/ProjetWeb/Modification/modificationparent.php'">
-<input type="button" value="Afficher vos données" onclick="window.location.href='http://localhost/Projet/GIT/ProjetWeb/Affichage/afficher.php'">
